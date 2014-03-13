e6799c832131a6a578bb35c86be725ec0356b63d
maskimko <maskimko@ukr.net>
2014-03-13 18:07:11 +0200

    Added module and breker population mothods

--- src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/util/Breaker.java
+++ src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/util/Breaker.java
@@ -6,6 +6,8 @@
 
 package ua.pp.msk.learn.javaee.cacti.jsf.util;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author maskimko
@@ -16,7 +18,36 @@ public class Breaker {
     //.1.3.6.1.4.1.318.1.1.22.2.4.1.1.1.1
     private int index;
     
-    class Phase{
+    private Phase[] phases = new Phase[3];
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Phase[] getPhases() {
+        return phases;
+    }
+
+    public void setPhases(Phase[] phases) {
+        this.phases = phases;
+    }
+    
+    public Phase getPhase(int index) {
+         if (index <0 || index > 2) {
+            Logger.getLogger(this.getClass().getName()).error("Invalid phase index " + index + "Must be between 0,1 or 2");
+            return null;
+        }
+        if (phases[index] == null) {
+            phases[index] = new Phase();
+        }
+        return phases[index];
+    }
+    
+    public class Phase{
         //PowerNet-MIB::isxModularDistModuleBreakerPhaseIndex.1.phaseL1 = INTEGER: phaseL1(1)
         //.1.3.6.1.4.1.318.1.1.22.2.4.1.2.1.1
         private byte index;
@@ -32,6 +63,46 @@ public class Breaker {
         //PowerNet-MIB::isxModularDistModuleBreakerPower.1.phaseL1 = INTEGER: -1
         //.1.3.6.1.4.1.318.1.1.22.2.4.1.11.1.1
         private float power;
+
+        public byte getIndex() {
+            return index;
+        }
+
+        public void setIndex(byte index) {
+            this.index = index;
+        }
+
+        public byte getPosition() {
+            return position;
+        }
+
+        public void setPosition(byte position) {
+            this.position = position;
+        }
+
+        public float getCurrent() {
+            return current;
+        }
+
+        public void setCurrent(float current) {
+            this.current = current;
+        }
+
+        public float getPercent() {
+            return percent;
+        }
+
+        public void setPercent(float percent) {
+            this.percent = percent;
+        }
+
+        public float getPower() {
+            return power;
+        }
+
+        public void setPower(float power) {
+            this.power = power;
+        }
         
         
     }
--- src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/util/ISXDevice.java
+++ src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/util/ISXDevice.java
@@ -6,20 +6,28 @@
 
 package ua.pp.msk.learn.javaee.cacti.jsf.util;
 
+import java.io.Serializable;
 import java.net.InetAddress;
-import java.util.Date;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
 
 /**
  *
  * @author maskimko
  */
-public class ISXDevice {
+public class ISXDevice implements Serializable{
+    
+   
+    
+    
     
     private InetAddress addr;
     private int port;
     private String snmpCommunity;
     private int snmpVersion;
+    //.1.3.6.1.2.1.1.6.0
     private String location;
+    //.1.3.6.1.2.1.1.5.0
     private String sysName;
     //PowerNet-MIB::isxModularDistSysVoltageLtoN.phase1
     //.1.3.6.1.4.1.318.1.1.22.4.1.15.1.3.1
@@ -57,23 +65,235 @@ public class ISXDevice {
     //.1.3.6.1.4.1.318.1.1.22.4.3.5.1.2.3
     private float sysPower3;
     
+    private  Breaker[] breaker = new Breaker[24];
+     private   Output[] output = new Output[24];
     
-    class ModuleOutput{
-        //PowerNet-MIB::isxModularDistModuleInfoModelNumber.1
-        //.1.3.6.1.4.1.318.1.1.22.2.2.1.3.1
-        private String model;
-        //PowerNet-MIB::isxModularDistModuleInfoSerialNumber.1
-        //.1.3.6.1.4.1.318.1.1.22.2.2.1.4.1
-        private String serialNumber;
-        
-        //PowerNet-MIB::isxModularDistModuleInfoDateOfManufacture.1 = ""
-        //.1.3.6.1.4.1.318.1.1.22.2.2.1.5.1
-        private Date manufactureDate;
-        
-        
-        Breaker[] breaker = new Breaker[24];
-        Output[] output = new Output[24];
+     public ISXDevice() {
     }
+    
+    public ISXDevice(InetAddress addr, int port, String snmpCommunity, int snmpVersion ){
+    
+        this.addr = addr;
+        this.port = port;
+        this.snmpCommunity = snmpCommunity;
+        this.snmpVersion = snmpVersion;
+    }
+    
+    public ISXDevice(String hostname, int port, String snmpCommunity, int snmpVersion ){
+        try {
+            this.addr = InetAddress.getByName(hostname);
+        } catch (UnknownHostException ue) {
+            Logger.getLogger(this.getClass().getName()).severe("Cannot parse hostname " + ue);
+        }
+        this.port = port;
+        this.snmpCommunity = snmpCommunity;
+        this.snmpVersion = snmpVersion;
+    }
+
+    public InetAddress getAddr() {
+        return addr;
+    }
+
+    public void setAddr(InetAddress addr) {
+        this.addr = addr;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getSnmpCommunity() {
+        return snmpCommunity;
+    }
+
+    public void setSnmpCommunity(String snmpCommunity) {
+        this.snmpCommunity = snmpCommunity;
+    }
+
+    public int getSnmpVersion() {
+        return snmpVersion;
+    }
+
+    public void setSnmpVersion(int snmpVersion) {
+        this.snmpVersion = snmpVersion;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSysName() {
+        return sysName;
+    }
+
+    public void setSysName(String sysName) {
+        this.sysName = sysName;
+    }
+
+    public float getVoltageLtoN1() {
+        return voltageLtoN1;
+    }
+
+    public void setVoltageLtoN1(float voltageLtoN1) {
+        this.voltageLtoN1 = voltageLtoN1;
+    }
+
+    public float getVoltageLtoN2() {
+        return voltageLtoN2;
+    }
+
+    public void setVoltageLtoN2(float voltageLtoN2) {
+        this.voltageLtoN2 = voltageLtoN2;
+    }
+
+    public float getVoltageLtoN3() {
+        return voltageLtoN3;
+    }
+
+    public void setVoltageLtoN3(float voltageLtoN3) {
+        this.voltageLtoN3 = voltageLtoN3;
+    }
+
+    public float getVoltageLtoL1() {
+        return voltageLtoL1;
+    }
+
+    public void setVoltageLtoL1(float voltageLtoL1) {
+        this.voltageLtoL1 = voltageLtoL1;
+    }
+
+    public float getVoltageLtoL2() {
+        return voltageLtoL2;
+    }
+
+    public void setVoltageLtoL2(float voltageLtoL2) {
+        this.voltageLtoL2 = voltageLtoL2;
+    }
+
+    public float getVoltageLtoL3() {
+        return voltageLtoL3;
+    }
+
+    public void setVoltageLtoL3(float voltageLtoL3) {
+        this.voltageLtoL3 = voltageLtoL3;
+    }
+
+    public float getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(float frequency) {
+        this.frequency = frequency;
+    }
+
+    public float getVoltageNominalLineToNeutral() {
+        return voltageNominalLineToNeutral;
+    }
+
+    public void setVoltageNominalLineToNeutral(float voltageNominalLineToNeutral) {
+        this.voltageNominalLineToNeutral = voltageNominalLineToNeutral;
+    }
+
+    public float getVoltageNominalLineToLine() {
+        return voltageNominalLineToLine;
+    }
+
+    public void setVoltageNominalLineToLine(float voltageNominalLineToLine) {
+        this.voltageNominalLineToLine = voltageNominalLineToLine;
+    }
+
+    public float getSysPower1() {
+        return sysPower1;
+    }
+
+    public void setSysPower1(float sysPower1) {
+        this.sysPower1 = sysPower1;
+    }
+
+    public float getSysPower2() {
+        return sysPower2;
+    }
+
+    public void setSysPower2(float sysPower2) {
+        this.sysPower2 = sysPower2;
+    }
+
+    public float getSysPower3() {
+        return sysPower3;
+    }
+
+    public void setSysPower3(float sysPower3) {
+        this.sysPower3 = sysPower3;
+    }
+
+    public Breaker[] getBreaker() {
+        return breaker;
+    }
+
+    public Breaker getBreaker(int index){
+         if (index <0 || index > 23) {
+            org.apache.log4j.Logger.getLogger(this.getClass().getName()).error("Invalid breaker index " + index + "Must be between 0 and 23");
+            return null;
+        }
+        return breaker[index];
+    }
+    public void setBreaker(Breaker[] breaker) {
+        this.breaker = breaker;
+    }
+
+    public Output[] getOutput() {
+        return output;
+    }
+
+    public Output getOutput(int index) {
+          if (index <0 || index > 23) {
+            org.apache.log4j.Logger.getLogger(this.getClass().getName()).error("Invalid output index " + index + "Must be between 0 and 23");
+            return null;
+        }
+          if (output[index] ==  null) {
+              output[index] = new Output();
+          }
+        return output[index];
+    }
+    public void setOutput(Output[] output) {
+        this.output = output;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + (this.addr != null ? this.addr.hashCode() : 0);
+        hash = 37 * hash + this.port;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ISXDevice other = (ISXDevice) obj;
+        if (this.addr != other.addr && (this.addr == null || !this.addr.equals(other.addr))) {
+            return false;
+        }
+        if (this.port != other.port) {
+            return false;
+        }
+        return true;
+    }
+    
+       
     
                
 }
