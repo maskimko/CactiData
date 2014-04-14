b3a34162ae66242b0fb38bf53d361d83b6859b21
maskimko <maskimko@ukr.net>
2014-04-14 18:20:06 +0300

    Split to subprojects

--- src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/IsxDeviceController.java
+++ src/main/java/ua/pp/msk/learn/javaee/cacti/jsf/IsxDeviceController.java
@@ -9,21 +9,13 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
-import javax.faces.model.SelectItem;
 import org.apache.log4j.Logger;
 import org.primefaces.model.chart.PieChartModel;
-import ua.pp.msk.learn.javaee.cacti.ejb.IsxDeviceFacade;
-import ua.pp.msk.learn.javaee.cacti.jsf.util.ISXDevice;
-import ua.pp.msk.learn.javaee.cacti.jsf.util.JsfUtil;
-import ua.pp.msk.learn.javaee.cacti.jsf.util.PaginationHelper;
+
+import ua.pp.msk.power.common.ISXDevice;
 
 /**
  *
